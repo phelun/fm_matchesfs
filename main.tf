c33008cde1c5687ff69e4a716a7e0dd45d923a84
phelun <phelun@yahoo.com>
2019-10-27 14:29:29 +0000

    MFS Works

--- main.tf
+++ main.tf
@@ -10,36 +10,36 @@ terraform {
 }
 
 locals {
-  users = ["nevsa", "cordelia", "kriste", "darleen",  "wynnie", "vonnie", "emelita", "maurita", "devinne", "breena"]
+  users = ["nevsa", "cordelia", "kriste", "darleen", "wynnie", "vonnie", "emelita", "maurita", "devinne", "breena"]
   envs  = ["dev", "qa", "uat", "test", "prod"]
 
   nestedloop = [
-      for user in local.users: [
-          for env in local.envs: [
-              join("-", [user, env])
-          ]
+    for user in local.users : [
+      for env in local.envs : [
+        join("-", [user, env])
       ]
+    ]
   ]
 }
 
 
 resource "aws_iam_user" "matches_map_users_env" {
-     for_each = toset(flatten(local.nestedloop))
-     name     = each.value
+  for_each = toset(flatten(local.nestedloop))
+  name     = each.value
 }
 
 resource "aws_iam_access_key" "matches_user_keys" {
-    for_each = toset(flatten(local.nestedloop))
-    user     = aws_iam_user.matches_map_users_env[each.value].name
+  for_each = toset(flatten(local.nestedloop))
+  user     = aws_iam_user.matches_map_users_env[each.value].name
 }
 
 ######################### OutPuts ###############################
 output "created_users" {
-  description  = "User List"
-  value = "${values(aws_iam_user.matches_map_users_env)[*].name}"
+  description = "User List"
+  value       = "${values(aws_iam_user.matches_map_users_env)[*].name}"
 }
 
 output "created_users_all_values" {
-  description   = "Detailed user info"
-  value = aws_iam_access_key.matches_user_keys
+  description = "Detailed user info"
+  value       = aws_iam_access_key.matches_user_keys
 }
